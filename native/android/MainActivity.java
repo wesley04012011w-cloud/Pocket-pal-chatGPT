96c006d56f09e7737011aa06379b3b10e1bbfd07
wesley04012011w-cloud <wesley04012011w@gmail.com>
2026-09-19 13:15:13 -0400

    Add native chat UI source

---
+++ native/android/MainActivity.java
@@ -0,0 +1,18 @@
+package dev.pocketpal.local;
+
+import android.app.*;
+import android.content.*;
+import android.graphics.*;
+import android.net.Uri;
+import android.os.*;
+import android.provider.OpenableColumns;
+import android.text.*;
+import android.view.*;
+import android.widget.*;
+import org.json.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class MainActivity {
+}
